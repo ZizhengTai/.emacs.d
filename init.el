b15f647ceba62a22f985ab7fa0ffc42acb0d06c1
Zizheng Tai <zizheng.tai@gmail.com>
2018-01-04 00:40:55 +0800

    Add things

--- init.el
+++ init.el
@@ -24,10 +24,13 @@
  '(inhibit-startup-screen t)
  '(neo-smart-open t)
  '(neo-theme (quote icons))
+ '(org-from-is-user-regexp "\\<Zizheng Tai\\>")
+ '(org-log-done (quote time))
  '(package-selected-packages
    (quote
-    (flycheck solarized-theme all-the-icons neotree alchemist evil smooth-scrolling)))
+    (smart-mode-line magit flycheck solarized-theme all-the-icons neotree alchemist evil smooth-scrolling)))
  '(require-final-newline t)
+ '(scroll-bar-mode nil)
  '(show-trailing-whitespace t)
  '(smooth-scroll-margin 1)
  '(smooth-scrolling-mode t)
@@ -38,7 +41,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Consolas"))))
+ '(custom-face-tag ((t (:inherit variable-pitch :foreground "#6c71c4" :weight normal :height 1.2 :family "Consolas"))))
+ '(custom-group-tag ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.2 :family "Consolas"))))
+ '(custom-group-tag-1 ((t (:inherit variable-pitch :foreground "#dc322f" :height 1.2 :family "Consolas"))))
+ '(custom-variable-tag ((t (:inherit variable-pitch :foreground "#2aa198" :height 1.2 :family "Consolas"))))
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "Consolas"))))
+ '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 1.2 :family "Consolas"))))
+ '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.15 :family "Consolas"))))
+ '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 1.1 :family "Consolas"))))
+ '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :family "Consolas"))))
+ '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :family "Consolas"))))
+ '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :family "Consolas"))))
+ '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :family "Consolas")))))
+
+(require 'company)
+(eval-after-load 'company
+  '(progn
+     (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+     (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)))
+(eval-after-load 'company
+  '(progn
+     (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
+     (define-key company-active-map (kbd "<backtab>") 'company-select-previous)))
+
+(require 'elixir-mode)
+(add-to-list 'elixir-mode-hook 'alchemist-mode)
 
 (require 'evil)
 (evil-mode 1)
@@ -47,12 +75,11 @@
 (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
 (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
 
-(eval-after-load 'company
-  '(progn
-     (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-     (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)))
+(require 'magit)
+(define-key global-map (kbd "C-x g") 'magit-status)
 
-(eval-after-load 'company
-  '(progn
-     (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
-     (define-key company-active-map (kbd "<backtab>") 'company-select-previous)))
+(require 'org)
+(define-key global-map (kbd "C-c l") 'org-store-link)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+
+(sml/setup)
