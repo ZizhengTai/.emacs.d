4e2a4cd6fc62e1483bf676533f00e81b672089b6
Zizheng Tai <zizheng.tai@gmail.com>
2018-01-14 00:36:03 -0800

    Update init.el

--- init.el
+++ init.el
@@ -13,10 +13,10 @@
     (company-pseudo-tooltip-unless-just-one-frontend company-echo-metadata-frontend company-preview-frontend)))
  '(company-idle-delay 0)
  '(company-require-match nil)
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("53f97243218e8be82ba035ae34c024fd2d2e4de29dc6923e026d5580c77ff702" default)))
  '(flycheck-display-errors-delay 0)
  '(global-company-mode t)
  '(global-flycheck-mode t)
@@ -28,12 +28,15 @@
  '(org-log-done (quote time))
  '(package-selected-packages
    (quote
-    (smart-mode-line magit flycheck solarized-theme all-the-icons neotree alchemist evil smooth-scrolling)))
+    (spaceline-all-the-icons spaceline monokai-theme magit flycheck all-the-icons neotree alchemist evil smooth-scrolling)))
  '(require-final-newline t)
  '(scroll-bar-mode nil)
  '(show-trailing-whitespace t)
  '(smooth-scroll-margin 1)
  '(smooth-scrolling-mode t)
+ '(spaceline-all-the-icons-hide-long-buffer-path t)
+ '(spaceline-all-the-icons-highlight-file-name t)
+ '(spaceline-all-the-icons-separator-type (quote arrow))
  '(tab-stop-list (quote (4 8)))
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -41,19 +44,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Consolas"))))
- '(custom-face-tag ((t (:inherit variable-pitch :foreground "#6c71c4" :weight normal :height 1.2 :family "Consolas"))))
- '(custom-group-tag ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.2 :family "Consolas"))))
- '(custom-group-tag-1 ((t (:inherit variable-pitch :foreground "#dc322f" :height 1.2 :family "Consolas"))))
- '(custom-variable-tag ((t (:inherit variable-pitch :foreground "#2aa198" :height 1.2 :family "Consolas"))))
- '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "Consolas"))))
- '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 1.2 :family "Consolas"))))
- '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.15 :family "Consolas"))))
- '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 1.1 :family "Consolas"))))
- '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :family "Consolas"))))
- '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :family "Consolas"))))
- '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :family "Consolas"))))
- '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :family "Consolas")))))
+ '(default ((t (:inherit nil :stipple nil :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Droid Sans Mono for Powerline"))))
+ '(custom-face-tag ((t (:inherit variable-pitch :foreground "#6c71c4" :weight normal :height 1.2 :family "Droid Sans Mono for Powerline"))))
+ '(custom-group-tag ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.2 :family "Droid Sans Mono for Powerline"))))
+ '(custom-group-tag-1 ((t (:inherit variable-pitch :foreground "#dc322f" :height 1.2 :family "Droid Sans Mono for Powerline"))))
+ '(custom-variable-tag ((t (:inherit variable-pitch :foreground "#2aa198" :height 1.2 :family "Droid Sans Mono for Powerline"))))
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "Droid Sans Mono for Powerline"))))
+ '(org-level-2 ((t (:inherit variable-pitch :foreground "#859900" :height 1.2 :family "Droid Sans Mono for Powerline"))))
+ '(org-level-3 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.15 :family "Droid Sans Mono for Powerline"))))
+ '(org-level-4 ((t (:inherit variable-pitch :foreground "#b58900" :height 1.1 :family "Droid Sans Mono for Powerline"))))
+ '(org-level-5 ((t (:inherit variable-pitch :foreground "#2aa198" :family "Droid Sans Mono for Powerline"))))
+ '(org-level-6 ((t (:inherit variable-pitch :foreground "#859900" :family "Droid Sans Mono for Powerline"))))
+ '(org-level-7 ((t (:inherit variable-pitch :foreground "#dc322f" :family "Droid Sans Mono for Powerline"))))
+ '(org-level-8 ((t (:inherit variable-pitch :foreground "#268bd2" :family "Droid Sans Mono for Powerline")))))
 
 (require 'company)
 (eval-after-load 'company
@@ -82,4 +85,5 @@
 (define-key global-map (kbd "C-c l") 'org-store-link)
 (define-key global-map (kbd "C-c a") 'org-agenda)
 
-(sml/setup)
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
