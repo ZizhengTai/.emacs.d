029dee7ce22081e99fabfd92fe55a3569c76bf72
Zizheng Tai <zizheng.tai@gmail.com>
2018-01-28 21:08:21 -0800

    Add font option

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono for Powerline" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal)))))
 
 (use-package alchemist
   :ensure t
