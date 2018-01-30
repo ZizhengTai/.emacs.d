e3f04a3587755d19052a3314da108b0599472afa
Zizheng Tai <zizheng.tai@gmail.com>
2018-01-30 13:49:37 -0800

    Delete trailing whitespace on save

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(before-save-hook (quote (delete-trailing-whitespace)))
  '(c-basic-offset 4)
  '(company-frontends
    (quote
